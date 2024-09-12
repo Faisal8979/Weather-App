9906c16814d72952ee3fa42a6d2dbae90691960f
Faisal8979 <faisal897913@gmail.com>
2024-09-13 02:52:49 +0530

    main

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Weather from './components/Weather';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const App = () => {
+  return (
+    <>
+<ToastContainer
+position="top-center"
+autoClose={3000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="light"
+/>
+    <div className='app'>
+        <Weather/>
+   </div>
+    </>
+  )
+}
+
+export default App;
